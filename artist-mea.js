7c9cc897e1359523d74222737396533951dd61e8
wonytest0-dev <wonytest0@gmail.com>
2026-05-17 01:08:34 +0700

    Update artist-mea.js

--- artist-mea.js
+++ artist-mea.js
@@ -116,7 +116,6 @@ weeklyJson
 
 };
 
-
 }
 
 async function scrape(
@@ -245,20 +244,93 @@ hasJimin
 
 ) {
 
+const currentRank =
+
+artist.chartEntryData
+?.currentRank;
+
+const previousRank =
+
+artist.chartEntryData
+?.previousRank;
+
+const rankChange =
+
+previousRank
+? Math.abs(
+currentRank -
+previousRank
+)
+: 0;
+
+let direction = "=";
+let entryStatus = null;
+
+// NEW ENTRY
+if (
+
+previousRank === null
+||
+previousRank === undefined
+
+) {
+
+entryStatus =
+"NEW_ENTRY";
+
+}
+
+// RE-ENTRY
+else if (
+
+rankChange >= 100
+
+) {
+
+entryStatus =
+"RE_ENTRY";
+
+}
+
+// NORMAL MOVEMENT
+else {
+
+if (
+
+currentRank <
+previousRank
+
+) {
+
+direction =
+"up";
+
+}
+
+else if (
+
+currentRank >
+previousRank
+
+) {
+
+direction =
+"down";
+
+}
+
+}
+
 results.push({
 
 country,
 type,
 
 rank:
-
-artist.chartEntryData
-?.currentRank,
+currentRank,
 
 previousRank:
-
-artist.chartEntryData
-?.previousRank,
+previousRank,
 
 peakRank:
 
@@ -278,7 +350,11 @@ artist.artistMetadata
 image:
 
 artist.artistMetadata
-?.displayImageUri
+?.displayImageUri,
+
+rankChange,
+direction,
+entryStatus
 
 });
 
@@ -447,4 +523,3 @@ console.log(
 }
 
 start();
-
